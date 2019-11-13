6cf1e3bc91fb627d63050f5a40e72c3cc49b5a7c
mustafamohsen97 <m.mohsen@campus.tu-berlin.de>
2019-11-13 16:46:21 +0100

    main scene test

--- src/tempus-fugit-client/game.ts
+++ src/tempus-fugit-client/game.ts
@@ -1,1 +1,23 @@
-document.body.innerText = "Hello World";
+import "phaser";
+import {MainScene} from "./scenes/main-scene";
+
+const config: Phaser.Types.Core.GameConfig = {
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    backgroundColor: "#efefef",
+    scene: [MainScene]
+};
+
+export class Game extends Phaser.Game {
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        super(config);
+    }
+}
+
+window.addEventListener("load", () => {
+    new Game(config);
+});
