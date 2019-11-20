db04fd23b6b350dafd82d3cfad67377ed905b149
dkejni <dkejni@gmail.com>
2019-11-20 03:07:53 +0100

    added phases, going to next phase triggers event

--- src/tempus-fugit-client/mechanics/gameloop.ts
+++ src/tempus-fugit-client/mechanics/gameloop.ts
@@ -1,24 +1,59 @@
+import EventEmitter = Phaser.Events.EventEmitter;
+
 export class Gameloop {
-    private readonly numPhases: 4;
+    private readonly numPhases: 6;
     private curPhase: number;
+    private emitter: EventEmitter;
+    private turnCount: number;
+    private toPhase: Map<number, string>;
 
     constructor() {
         this.curPhase = 0;
+        this.emitter = new EventEmitter();
+        this.turnCount = 0;
+
+        this.toPhase = new Map<number,string>();
+        this.toPhase.set(0, 'draw-phase');
+        this.toPhase.set(1, 'energy-phase');
+        this.toPhase.set(2, 'attack-phase');
+        this.toPhase.set(3, 'effect-phase');
+        this.toPhase.set(4, 'enemy-attack-phase');
+        this.toPhase.set(5, 'enemy-effect-phase');
     }
 
     /**
      * Switches phases
-     * 0 -> Energy Phase
-     * 1 -> Card Phase
-     * 2 -> Effect Phase
-     * 3 -> Enemy Phase
+     * --- Player's turn ---
+     * 0 -> Draw Phase
+     * 1 -> Energy Phase
+     * 2 -> Play/Attack Phase
+     * 3 -> Effect Phase (Player or Enemy)
+     * --- Enemy's turn ---
+     * 4 -> Attack Phase
+     * 5 -> Effect Phase
+     *
+     * emits an event for each phase, names can be seen in toPhase map
+     * increments turn counter every time player turn is reached
      */
-    nextPhase() {
+    public nextPhase() {
         this.curPhase = (this.curPhase + 1) % this.numPhases;
+        let event = this.toPhase.get(this.curPhase);
+        this.emitter.emit(event);
+        if (this.curPhase === 0) {
+            this.incrementTurnCount();
+        }
     }
 
-    getPhase() {
+    public getPhase() {
         return this.curPhase;
+    }
+
+    public resetTurnCount() {
+        this.turnCount = 0;
+    }
+
+    public incrementTurnCount() {
+        this.turnCount++;
     }
 
 }
