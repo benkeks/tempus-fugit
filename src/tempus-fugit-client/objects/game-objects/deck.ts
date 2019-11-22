5d3276fca86b23d75f88bd7b3484858e26567406
Kejni Dema <dkejni@gmail.com>
2019-11-22 13:46:50 +0000

    Revert "Merge branch 'revert-9f32c683' into 'master'"
    This reverts merge request !14

---
+++ src/tempus-fugit-client/objects/game-objects/deck.ts
@@ -0,0 +1,54 @@
+import {Card} from "./card";
+import {PlayerListener} from "./player";
+
+export class Deck {
+    cards: Card[]; // List of cards contained in the deck
+    listener:DeckListener[]; // List of objects listening to events happening in the deck
+
+    /**
+     * Constructor for the Deck class (creates an empty deck)
+     * @author Florian
+     */
+    constructor() {
+        this.cards = [];
+        this.listener = []
+    }
+
+    /**
+     * Puts the card on top of the deck and informs deck listeners
+     * @param card The card that is to be added to the deck
+     * @return No return value
+     * @example someDeck.addCard(dummyCard);
+     * @author Florian
+     */
+    addCard(card: Card): void {
+        this.cards.push(card);
+        for (let i in this.listener) {
+            this.listener[i].numCardsChanged(this.cards.length);
+        }
+    }
+
+    /**
+     * Returns the card on top of the deck (i.e. the one that has been in the deck the longest)
+     * @example myCard = someDeck.takeCardOnTop();
+     * @return Returns the card on top of the deck
+     * @example myNewCard = someDeck.takeCardOnTop();
+     * @author Florian
+     */
+    takeCardOnTop(): Card {
+        var theCard = this.cards.shift();
+        for (let i in this.listener) {
+            this.listener[i].numCardsChanged(this.cards.length);
+        }
+        return theCard;
+    }
+
+}
+
+/**
+ * Interface for objects that listen to changes in deck objects
+ * @author Florian
+ */
+export interface DeckListener {
+    numCardsChanged(numCards: number): void;
+}
