8dd3813baaec25240fc504f18be13132eed545e3
florian.eyert <florian.eyert@tu-berlin.de>
2019-11-27 19:26:39 +0100

    creates the stand class with all relevant attributes and methods

---
+++ src/tempus-fugit-client/objects/game-objects/stand.ts
@@ -0,0 +1,47 @@
+import {Player} from "./player"
+import {Card} from "./card"
+import {Enemy} from "./enemy"
+import {GameState} from "./game-state"
+
+
+export class Stand {
+    private card: Card; // The card that is associated with the stand
+    private roundsRemaining: number; // The rounds that the stand will still be alive
+    public targets: Enemy[]; // A list of targets the stand will attack
+
+    // Getter method for the card attribute
+    public getCard(): Card {
+        return this.card;
+    }
+
+    // Getter method for the roundsRemaining attribute
+    public getRoundsRemaining(): number {
+        return this.roundsRemaining;
+    }
+
+    constructor(card: Card, roundsActive: number, targets: Enemy[]) {
+        this.card = card;
+        this.roundsRemaining = roundsActive;
+        this.targets = targets;
+    }
+
+
+    public attackTargets(gameState: GameState): void {
+        for (var target of this.targets) {
+            if (gameState.evaluate(this.card)) {
+                target.takeHit(this.card.getAttackPower())
+            }
+        }
+        this.roundsRemaining -= 1;
+    }
+
+}
+
+
+    /**
+     * Interface for objects that listen to changes in enemy objects
+     * @author Florian
+     */
+export interface EnemyListener {
+    remainingRoundsChanged(changedTo: number): void;
+}
