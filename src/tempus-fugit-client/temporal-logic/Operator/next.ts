61ae7e36cc394232b257556d08a5123b56c47df8
Tobias Loch <Tobias.Loch@mail.de>
2026-04-28 14:53:15 +0200

    adding comments for infinite trace logic

--- src/tempus-fugit-client/temporal-logic/Operator/next.ts
+++ src/tempus-fugit-client/temporal-logic/Operator/next.ts
@@ -15,6 +15,9 @@ export class Next extends OneParamOperator {
     evaluateInternal(condition: number, direction:number=Proposition.DEFAULT_DIRECTION): PropositionStatus {
         let status:PropositionStatus = this.operand.evaluateInternal(condition+direction, Proposition.DEFAULT_DIRECTION);
 
+        // this is heuristically, because O#O (next applied on reverse next) is not cancelling itself out,
+        // but since this is only needed to bound computation time
+        // this was sufficient enough
         if (direction > 0) {
             status.minStatus--;
         } else {
